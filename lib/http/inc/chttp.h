7d8680e64fa182f31f5b9bf9c1a7b78a5e575a31
Chiming0926 <Chiming0926@gmail.com>
2015-08-05 10:13:03 +0800

    Add chttp lib

---
+++ lib/http/inc/chttp.h
@@ -0,0 +1,6 @@
+#ifndef _CHTTP_H_
+#define _CHTTP_H_
+
+extern void init_chttp(void);
+
+#endif
---
+++ lib/http/src/chttp.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void init_chttp(void)
+{
+	printf("@@@@@@ chttp init @@@@@@ \n");
+}
+
